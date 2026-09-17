c664f4459ba3bc2ac52b6d4cbad085d70053ef23
labkomputeruinssc-arch <labkomputeruinssc@gmail.com>
2026-09-17 10:03:16 +0700

    feat: add practical report 3, assets, and core component structures in App.js

--- Pem-Mobile/Pertemuan-2/ptmn2/App.js
+++ Pem-Mobile/Pertemuan-2/ptmn2/App.js
@@ -1,12 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Langkah 1 Import Core Components dan Library
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  FlatList,
+  SectionList,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Pressable,
+  Switch,
+  Modal,
+  ActivityIndicator,
+  StatusBar,
+  SafeAreaViewBase,
+  StyleSheet,
+  Platform
+} from 'react-native';
+
+const PROFILE = {
+  name: 'Mohamad Firdaus',
+  title: 'Full-Stack Mobile Developer',
+  email: 'mohamadfirdaus@uinssc.ac.id',
+  phone: '0821-7073-5116',
+  location: 'Cirebon, West Java',
+  bio: 'Pengembang aplikasi mobile berpengalaman 4 tahun yang berfokus di React Native dan Flutter',
+  avatar: 'https://lh3.googleusercontent.com/ogw/AF2bZyi8uF-5bgqEOwd31f__38Ysl9uXf4ufL26Rcv-1bAW4aJM=s64-c-mo',
+  avatarOffline: 'assets/foto.png'
+};
+
+const SKILLSS = [
+  { id: '1', name: 'React Native', level: 90, color: '#61DAFB' },
+  { id: '2', name: 'Flutter', level: 75, color: '#02569C' },
+  { id: '3', name: 'Javascript', level: 88, color: '#F7DF1E' },
+  { id: '4', name: 'Typescript', level: 80, color: '#3178C6' },
+  { id: '5', name: 'Node JS', level: 70, color: '#339933' },
+  { id: '6', name: 'Firebase', level: 82, color: '#FFCA28' },
+  { id: '7', name: 'Tailwind CSS', level: 95, color: '#06B6D4' },
+];
+
+const SECTIONS = [
+  {
+    title: '💼 Pengalaman Kerja',
+    data: [
+      {
+        id: 'e1',
+        role: 'Senior Mobile Developer',
+        company: 'PT. Tech Solutions',
+        period: 'Januari 2022-Sekarang',
+        desc: 'Mengembangkan dan memelihara aplikasi mobile menggunakan React Native dan Flutter'
+      },
+      {
+        id: 'e2',
+        role: 'Mobile Developer',
+        company: 'PT. Inovasi Digital',
+        period: 'Januari 2022-Sekarang',
+        desc: 'Mengembangkan dan memelihara aplikasi mobile menggunakan React Native dan Flutter'
+      },
+    ],
+  },
+  {
+    title: "🎓 Pendidikan",
+    data: [
+      {
+        id: 'd1',
+        role: 'S1 Informatika',
+        company: 'Universitas Islam Negeri Siber Syekh Nurjati Cirebon',
+        period: '2024-2029',
+        desc: 'IPK 3.75 / 4.00 | Skripsi: Implementasi Machine Learning pada Applikasi Mobile'
+      }
+    ]
+  }
+];
+
+const SOCIAL = [
+  { id: 's1', label: 'Github', icon: '🎖️', url: 'https://github.com/labkomputeruinssc-arch' },
+  { id: 's2', label: 'LinkedIn', icon: '💼', url: 'https://www.linkedin.com/in/mohamadfirdaus-/' },
+  { id: 's3', label: 'Youtube', icon: '📹', url: 'https://www.youtube.com/@amohfi' },
+]
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Nama Lengkap: Mohamad Firdaus</Text>
+      <Text>Tempat Tanggal Lahir: Tangerang, 04 April 2005</Text>
+      <Text>Cita-Cita: Menjadi Digital Marketer yang sukses</Text>
+      <Text>Nama Lengkap: Mohamad Firdaus</Text>
+      <Text>Tempat Tanggal Lahir: Tangerang, 04 April 2005</Text>
+      <Text>Cita-Cita: Menjadi Digital Marketer yang sukses</Text>
+      <Text>Nama Lengkap: Mohamad Firdaus</Text>
+      <Text>Tempat Tanggal Lahir: Tangerang, 04 April 2005</Text>
+      <Text>Cita-Cita: Menjadi Digital Marketer yang sukses</Text>
+      <Text>Nama Lengkap: Mohamad Firdaus</Text>
+      <Text>Tempat Tanggal Lahir: Tangerang, 04 April 2005</Text>
+      <Text>Cita-Cita: Menjadi Digital Marketer yang sukses</Text>
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
---
+++ Pem-Mobile/Pertemuan-3/Laporan-Praktikum-3.md
@@ -0,0 +1,37 @@
+# Laporan Praktikum Pemrograman Mobile Pertemuan 3 #
+
+### Pengenalan Core Component dan Styling ###
+
+## 🎯 Tujuan Pembelajaran
+
+Setelah menyelesaikan praktikum ini, mahasiswa mampu:
+
+1. Memahami dan menggunakan **16 Core Components** React Native
+2. Menerapkan **StyleSheet** untuk styling terpusat
+3. Menggunakan **useState** untuk state management dasar
+4. Membuat layout yang responsif dengan **Flexbox**
+5. Menangani **interaksi pengguna** (tekan, input, scroll)
+
+Langkah 1: Mengimpor Components
+
+1. Buka File App.js pada folder projek ptmn2
+2. Import 16 core components sebagai berikut:
+3. Konfirmasi Bukti
+![alt text](image.png)
+
+Langkah 2: Menyiapkan data Objek dan Array 
+1. Membuat Objek untuk menyimpan data Profile
+2. Buat Objek Array bernama PROFILE
+3. Konfirmasi Bukti
+![alt text](image-2.png)
+4. Membuat Objek Array SKILSS untuk menyimpan data SKills
+5.Buat Objek Array bernama SKILLSS
+6. Konfirmasi Bukti
+![alt text](image-3.png)
+7. Membuat Objek Array SECTIONS untuk menyimpan Riwayat Pekerjaan 
+8. Membuat Objek Array SOCIAL untuk menyimpan Media Sosial
+9. Konfirmasi Bukti SECTIONS
+![alt text](image-4.png)
+10. Konfirmasi Bukti SOCIAL
+![alt text](image-5.png)
+
